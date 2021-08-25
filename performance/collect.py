9478e460ac1de3247b576a4dba43d830077457f2
toxa81 <toxa81@gmail.com>
2021-08-25 13:57:53 +0200

    collect energy

--- performance/collect.py
+++ performance/collect.py
@@ -15,7 +15,6 @@ def get_qe_time(fname):
     result = 0
     for line in f.readlines():
         # PWSCF        :  27m49.03s CPU  11m50.52s WALL
-        #m = re.search('\s+PWSCF\s+:(\w+)CPU(\w+)WALL', line)
         m = re.search('\s+PWSCF\s+:(.*)CPU(.*)WALL', line)
         # match is successful
         if m:
@@ -36,6 +35,14 @@ def get_qe_time(fname):
     f.close()
     return result
 
+def get_qe_energy(fname):
+    f = open(fname, 'r').read()
+    m = re.search('Username.*\n.*\n\s*(\w*)\s*(\w*)\s*(\w*)\s*(\w*)\s*([0-9]*\.[0-9]*)\skJ', f)
+    if m:
+        return float(m.group(5))
+    else:
+        return 0
+
 def main():
 
     title = os.path.basename(args.DIR)
@@ -43,30 +50,29 @@ def main():
     print(f"Looking in {args.DIR}")
 
     results = {}
+    results["data"] = {}
 
     for root, label, files in os.walk(args.DIR):
         for f in files:
             if f == 'slurm-stdout.txt':
                 p, task_id = os.path.split(root)
                 p, label = os.path.split(p)
-                print(task_id)
-                print(label)
-                print(root)
-                tts = get_qe_time(f"{root}/slurm-stdout.txt")
                 m = re.search('(\d+)N_(\d+)R_(\d+)T', task_id)
                 N = int(m.group(1))
-                print(f"nodes: {N}")
-                if label not in results:
-                    results[label] = []
-                results[label].append((N, tts))
+                print(f"nodes: {N}, task layout: {task_id}")
+                if label not in results["data"]:
+                    results["data"][label] = []
+                scf_time = get_qe_time(f"{root}/slurm-stdout.txt")
+                energy = get_qe_energy(f"{root}/slurm-stdout.txt")
+                r = {'nodes' : N, 'scf_time' : scf_time, 'energy' : energy,
+                     'ranks' : int(m.group(2)), 'threads_per_rank' : int(m.group(3))}
+                results["data"][label].append(r)
 
-    sorted_results = {}
-    for e in results:
-        sorted_results[e] = sorted(results[e])
-
-
-    with open("output.json", "w") as out:
-        json.dump(sorted_results, out)
+    results['title'] = title
+    results['energy_units'] = 'kJ'
+    results['time_units'] = 's'
+    with open(f"{title}.json", "w") as out:
+        out.write(json.dumps(results,  indent=4))
 
     return
 
